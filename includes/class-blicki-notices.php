48acf82e56adfcd3be93cc516d65ddfcf63c46f7
Benedict Singer <singerb@mac.com>
2016-09-19 11:11:16 -0700

    Add an admin notice when there are pending suggestions.

--- includes/class-blicki-notices.php
+++ includes/class-blicki-notices.php
@@ -7,12 +7,18 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Handles notice output.
  */
 class Blicki_Notices {
-
     /**
      * Stores notices.
      * @var array
      */
     private static $notices = array();
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+		add_action( 'admin_notices', array( $this, 'add_pending_suggestions_notice' ) );
+    }
 
     public static function add( $notice_text, $type = 'error' ) {
         self::$notices[] = (object) array(
@@ -29,4 +35,30 @@ class Blicki_Notices {
         }
     }
 
+	public function add_pending_suggestions_notice() {
+		$suggestions = get_posts( array(
+            'fields'         => 'ids',
+			'post_type'      => 'blicki-suggestion',
+            'posts_per_page' => -1,
+            'post_status'    => 'pending',
+        ) );
+
+		$num = count( $suggestions );
+		if ( $num > 0 ) {
+			?>
+			<div class="notice notice-info"><p>
+				<?php
+				printf( _n(
+					'Blicki - There is %d pending suggestion to approve',
+					'Blicki - There are %d pending suggestions to approve',
+					$num,
+					'blicki' ),
+				$num );
+				echo " - <a href='" . admin_url( 'edit.php?post_type=blicki&orderby=pending_suggestions&order=desc' ) . "'>" . __( 'Entries', 'blicki' ) . "</a>";
+				?>
+			</p></div>
+			<?php
+		}
+	}
 }
+new Blicki_Notices();
